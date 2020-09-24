2585b6c443e0a377be214bfe6597530f8ea354c1
Abdihakin Elmi <Abdihakiin5452@gmail.com>
2020-09-24 10:58:51 +0530

    Initial Commit

---
+++ Remainder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Remainder
+//
+//  Created by ABDIHAKIN ELMI on 24/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
